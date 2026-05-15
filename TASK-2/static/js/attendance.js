c37ef2fef762c6844a95fb9576ed18582dfefa0a
vandhana-14 <vandhanarkm@gmail.com>
2026-05-15 19:13:30 +0530

    Delete TASK-2 directory

--- TASK-2/static/js/attendance.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const dateInput = document.getElementById("attendance-date");
-  if (!dateInput.value) {
-    dateInput.value = new Date().toISOString().split("T")[0];
-  }
-});
